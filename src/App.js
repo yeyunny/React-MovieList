75c2929348c51d2195c906c2e2c11bf293608712
yeyun <hallo.yunye@gmail.com>
2023-06-23 16:37:42 +0900

    [ADD] useEffect() 동시에 사용해보기

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("CALL the API");
+    console.log("i run only once");
   }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when keyword & counter changes");
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me </button>
     </div>
