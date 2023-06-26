ae75b56c497fe33a5a1426790a574ca0c71724bf
yeyun <hallo.yunye@gmail.com>
2023-06-26 13:55:19 +0900

    [ADD] useState, preventDefault 사용하여 toDoList 만들기

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  //방법1 - 추천
-  // useEffect(function () {
-  //   console.log("hi :)");
-  //   return function () {
-  //     console.log("bye :(");
-  //   };
-  // }, []);
-
-  //방법2 - 추천
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          text="text"
+          placeholder="Write your to do.."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
